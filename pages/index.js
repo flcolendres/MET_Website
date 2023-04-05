9b70f0c38f47671591874a00f4e01353b5054f0b
Francis Colendres <flcolendres@myseneca.ca>
2023-04-05 18:38:47 -0400

    Modifying the files

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 *
-* Name: Francis Lloyd Colendres Student ID: 145498200 Date: March 24, 2023
+* Name: Francis Lloyd Colendres Student ID: 145498200 Date: April 07, 2023
 *
 ********************************************************************************/
 
